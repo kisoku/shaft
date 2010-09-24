8b4ad243bea163971ca9b7136e8951030cc09878
Mathieu Sauve-Frankel <msf@kisoku.net>
2010-09-24 22:22:29 +0900

    remove old comments from sftp-client.c

--- shaft-client.c
+++ shaft-client.c
@@ -16,11 +16,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* XXX: memleaks */
-/* XXX: signed vs unsigned */
-/* XXX: remove all logging, only return status codes */
-/* XXX: copy between two remote sites */
-
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/stat.h>
